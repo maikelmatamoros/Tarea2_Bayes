3c36ad25162d5893e546035054e2a97c795c30e3
maikelmatamoros <37597397+maikelmatamoros@users.noreply.github.com>
2022-06-11 16:52:25 -0600

    DataBaseUpdate

--- libs/configuration.php
+++ libs/configuration.php
@@ -9,7 +9,7 @@
     $config->set('dbhost', "163.178.107.10");
     $config->set('dbname', 'DBTarea2ExpertosB64219');
     $config->set('dbuser', 'laboratorios');
-    $config->set('dbpass', "KmZpo.2796");
+    $config->set('dbpass', "Uy&)&nfC7QqQau.%278UQ24/=%");
 
     
     
